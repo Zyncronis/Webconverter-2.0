34e77ea6afbd985a284405b258d8089b501eadd8
Vetle Lode <vetlelode@gmail.com>
2016-04-08 13:27:53 +0200

    Re configed the server

--- Serverside/Node/Main.js
+++ Serverside/Node/Main.js
@@ -7,8 +7,8 @@ var readline = require('readline');
 
 
 //app.use(express.compress()); ## option for compressing with gzip
-app.use(express.static(path.join(__dirname, '../../Website'))); // tells express where to look for the static files
+app.use(express.static(path.join(__dirname, '../../'))); // tells express where to look for the static files
 app.listen(process.env.PORT || 1706);
 
 
-console.log('Server running at http://127.0.0.1:1706/');
+console.log('Server running at http://192.168.1.69:1706');
