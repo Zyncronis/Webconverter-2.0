90e275951c410da60e965bb1c7357225b8ae747f
Vetle Lode <vetlelode@gmail.com>
2016-04-14 00:41:42 -0700

    Updated the upload.php

--- upload.php
+++ upload.php
@@ -1,4 +1,20 @@
 <?php
-    echo "i live";
+    echo "i fck ur mum";
+    $target_dir ="uploads/"
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
+    $uploadOK = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXSTENSION);
+    if(isset($_POST["submit"])){
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== FALSE){
+            echo "File is an image -" . $check["mime"] . ".";
+            $uploadOK = 1;
+        } else{
+            echo "File is not an image brav"; 
+            $uploadOK =0; $
+        }
+
+    } 
    
 ?>
